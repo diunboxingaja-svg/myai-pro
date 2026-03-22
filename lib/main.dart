506b8daa305c9353f997e01332636b39dbeb6e51
diunboxingaja-svg <diunboxing.aja@gmail.com>
2026-03-22 10:24:59 +0700

    Create main.dart for MyAI Flutter application
    Initial Flutter app setup with a basic UI.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyAI());
+}
+
+class MyAI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("MyAI PRO")),
+        body: Center(
+          child: Text("APK SUCCESS"),
+        ),
+      ),
+    );
+  }
+}
